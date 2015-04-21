6446b74c830503c4064a09b17f2e68039ac015ec
KoinoAoi <KoinoAoi@gmail.com>
2015-04-21 13:26:23 -0400

    filename stuff

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ $(document).on('click', '.image', function(event) {
 	event.preventDefault();
 	var $el = $(event.target),
 		href = $el[0].href;
-
+		console.log(href);
 	/*
 	 * There is not enough information here. We will have to download and
 	 * parse the page.
@@ -29,17 +29,16 @@ $(document).on('click', '.image', function(event) {
 	$.get(href, function(data) {
 		if (!data)
 			return console.error('Failed to fetch: ' + href);
+		var artID = href.match(/=(\d+)/)[1];
 		var m = data.match(/<[^>]*original-image"[^>]*>/); //check if single image
 		var type = 'single';
 		var pageCount = 1;
 		if (!m){//if not single, check for manga
 			m = data.match(/<[^>]*mode=manga([^<]*)/);
-			
 			if(!m)//if not manga, end
 				return console.error('Parsing error: ' + href);
 			type = 'manga';
 			pageCount = parseInt(data.match(/(\d+)(?=P<\/)/)[0]);
-			console.log(pageCount);
 
 		}
 
@@ -47,13 +46,16 @@ $(document).on('click', '.image', function(event) {
 		if (type == 'manga'){
 				url = $origin[0].href;
 				url = url.replace('manga', 'manga_big').concat('&page=0');
-				var d = new Date();
 				var mangaxhr = new XMLHttpRequest();
 				//synchronous loading is scary, but we only need the page=0 to get the URL
 				mangaxhr.open('GET', url); 
 				mangaxhr.send();
-				url = mangaxhr.responseText.match(/http:\/\/i\d[^"]*/)[0];
-				console.log('mangadata: ' +url);					
+				mangaxhr.onload = function(){
+					console.log('get');
+					url = mangaxhr.responseText.match(/http:\/\/i\d[^"]*/)[0];
+					proceedToDownload();
+				}
+					
 			}
 		else
 			url = $origin.data('src');
@@ -62,36 +64,36 @@ $(document).on('click', '.image', function(event) {
 		content = info[0].content;
 		title = content.match(/[^|]*/)[0];
 		artist = content.match(/[|][^[]*/)[0].substring(2);
-		console.log('url: '+ url);
-		console.log('title: '+ title);
-		console.log('artist: ' + artist);
-
-
+		if (type == 'single')
+			proceedToDownload();
 		// Fetch the image as a Blob
 		//iterate through every page number if it's a manga. If not, it'll stop after the first.
-		for(var x=0; x<pageCount;x++){
-			var xhr = new XMLHttpRequest();
-			var name = artist + ' - ' + title;
-			if(type='manga'){
-				//Adjust Filename
-				url=url.replace(/_p\d/, '_p'+x);
-				console.log('Iterated URL: '+url);
-				name.concat(' ('+ x +')');
+		function proceedToDownload(){
+			for(var x=0; x<pageCount;x++){
+				var xhr = new XMLHttpRequest();
+				var name = artist + ' - ' + title +'('+artID+')';
+				if(type='manga'){
+					//Adjust Filename
+					url=url.replace(/_p\d/, '_p'+x);
+					console.log('Iterated URL: '+url);
+					name = name.concat(' pg'+ x);
+
+				}
+
+				xhr.open('GET', url);
+				xhr.responseType = 'blob';
+				xhr.send();
+				xhr.onload = function(){
+					if (this.status !== 200)
+						return console.error(`Fetch failed: ${this.status} : ${url}`);
+					// Download the Blob
+					var a = document.createElement('a');
+					a.setAttribute('href',window.URL.createObjectURL(this.response));
+					a.setAttribute('download', name);
+					a.click();
+				};			
 			}
-			xhr.open('GET', url);
-			xhr.responseType = 'blob';
-			xhr.send();
+		}// end of proceedToDownload()
 
-			xhr.onload = function(){
-				if (this.status !== 200)
-					return console.error(`Fetch failed: ${this.status} : ${url}`);
-				// Download the Blob
-
-				var a = document.createElement('a');
-				a.setAttribute('href',window.URL.createObjectURL(this.response));
-				a.setAttribute('download', name);
-				a.click();
-			};			
-		}
 	});
 });
